306a59f293b69e9fbe4a48c8bb3ccd923fba419f
sujitdhamke33 <sujitdhamke12@gmail.com>
2024-03-15 22:49:33 +0530

    Master created

---
+++ Addressbook.java
@@ -0,0 +1,8 @@
+package AddressBook;
+
+public class Addressbook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book\n" +
+                "Program");
+    }
+}
